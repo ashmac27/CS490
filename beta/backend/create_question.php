6f60e058291ba63b9128c097ff979b544cfe70ee
ashmac27 <44986581+ashmac27@users.noreply.github.com>
2020-06-03 16:49:18 -0400

    Update create_question.php

--- beta/backend/create_question.php
+++ beta/backend/create_question.php
@@ -12,23 +12,22 @@ if($db_conn-> connect_error)
 
 $response = "fail";
 
-//$qid = mysqli_real_escape_string($_POST['qid']);
 $topic = mysqli_real_escape_string($_POST['topic']);
 $difficulty = mysqli_real_escape_string($_POST['difficulty']);
-//$input = mysqli_real_escape_string($_POST['input']);
-//$output = mysqli_real_escape_string($_POST['output']);
 $question = mysqli_real_escape_string($_POST['question']);
 $tc1 = mysqli_real_escape_string($_POST['testcase1']);
 $tc2 = mysqli_real_escape_string($_POST['testcase2']);
+$answer1 = mysqli_real_escape_string($_POST['answer1']);
+$answer2 = mysqli_real_escape_string($_POST['answer2']);
 
-$question_query = "INSERT INTO Questions (topic, difficulty, question, tc1, tc2)
-                    VALUES ('$topic','$difficulty','$question', '$tc1', '$tc2');";
+$question_query = "INSERT INTO Questions (topic, difficulty, question, tc1, tc2, answer1, answer2)
+                    VALUES ('$topic','$difficulty','$question', '$tc1', '$tc2', '$answer1','$answer2')";
 
 if($db_conn->query($question_query))
     $response = "Success! You created a question";
 
 $json_response = json_encode($response);
-echo $json_response
+echo $json_response;
 
 $db_conn->close();
 ?>
