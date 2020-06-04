d177bd2c9e3e370bd8bdd7bbbc5d1403b1932e9e
ashmac27 <44986581+ashmac27@users.noreply.github.com>
2020-06-03 21:46:31 -0400

    Update createExam.php

--- beta/backend/createExam.php
+++ beta/backend/createExam.php
@@ -11,13 +11,10 @@ $db_conn = mysqli_connect ($host, $user, $dbpassword, $db);
 if($db_conn-> connect_error)
     die("Connection failure: ". $db_conn -> connect_error);
 
-$response = "fail";
-$exam = $_POST['exam_title'];
-//question bank should be an unordered list with the id question_bank
-$questions_array = $_POST['questions'];
-$points = $_POST['points'];
 
-//foreach($i in $question_array_str)
+$exam = $_POST['exam_title'];
+$questions_array = unserialize($_POST['questions']);
+$points = unserialize($_POST['points']);
 
 
 $query = "SELECT username FROM login WHERE role ='student';";
@@ -26,36 +23,22 @@ $result = $db_conn->query($query);
 $students_selected = [];
 
 while($row = $result->fetch_assoc()){
-    array_push($students_assigned, $row['username']);
+    array_push($students_selected, $row['username']);
 }
 
 $student_status = "ungraded";
-//$exam_status = "undelivered";
-foreach ($students_assigned as $username) {
+foreach ($students_selected as $username) {
     $exam_roster_insert = "INSERT INTO ExamRoster(username, exam, status)
                             VALUES('$username','$exam','$student_status');";
     $erq = $db_conn->query($exam_roster_insert);
 }
 
-//insert exam title, array of all the questions, array of all the points related 
-//to questions, insert exam status (not yet delivered)
 $add_exam = "INSERT INTO Exams (exam, questions, points)
              VALUES ('$exam','$questions_array','$points')";
 $aeq = $db_conn->query($add_exam);
 if($aeq ==TRUE){
     echo json_encode("You created an exam");
 }
-
-//if teacher decides to click to send the exam to students
-//if($_POST['deliver_exam']){
-  //  $deliver_exam_q = "UPDATE ExamRoster SET status = 'delivered' WHERE exam = '$exam'";
-    //$deq = $db_conn->query($deliver_exam_q);
-//}
-
-//if($deq == TRUE){
-  //  $response = "You sent the exam!";
-   // echo json_encode($response);
-//}
 
 $db_conn->close();
 ?>
