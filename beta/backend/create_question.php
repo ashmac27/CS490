6f7e237cccf237de33078b9a822f1e7f6c34c39c
ashmac27 <44986581+ashmac27@users.noreply.github.com>
2020-06-03 23:20:58 -0400

    Update create_question.php

--- beta/backend/create_question.php
+++ beta/backend/create_question.php
@@ -10,26 +10,23 @@ $db_conn = mysqli_connect ($host, $user, $dbpassword, $db);
 if($db_conn-> connect_error)
     die("Connection failure: ". $db_conn -> connect_error);
 
-//$response = "fail";
 
 $topic = $_POST['topic'];
 $difficulty = $_POST['difficulty'];
 $question = $_POST['question'];
 $tc1 = $_POST['testcase1'];
 $tc2 = $_POST['testcase2'];
-$answer1 = $_POST['answer1']);
+$answer1 = $_POST['answer1'];
 $answer2 = $_POST['answer2'];
 
-$question_query = "INSERT INTO Questions (topic, difficulty, question, tc1, tc2, answer1, answer2) VALUES ('$topic','$difficulty','$question', '$tc1', '$tc2', '$answer1','$answer2');";
-echo $question_query;
+$question_query = "INSERT INTO Questions (topic, difficulty, question, tc1, tc2, answer1, answer2) 
+            VALUES ('$topic','$difficulty','$question', '$tc1', '$tc2', '$answer1','$answer2');";
 $question_ex = $db_conn->query($question_query);
-//echo $question_ex;
 
-//if($db_conn->query($question_query))
-  //  $response = "Success! You created a question";
+
 if($question_ex == TRUE){
     $json_response = "Question Created!";
-    echo $json_response;
+    echo json_encode($json_response);
 }
 
 $db_conn->close();
