9037bb0ea99d64c9ffed4f47eb4e10ccbe5f00f5
ashmac27 <44986581+ashmac27@users.noreply.github.com>
2020-06-04 11:23:38 -0400

    Update reviewStudents.php

--- beta/backend/reviewStudents.php
+++ beta/backend/reviewStudents.php
@@ -6,19 +6,23 @@ $host = "sql1.njit.edu";
 $user = "am2829";
 $dbpassword = "urLNjFMv";
 $db  = "am2829";
-
-//Connecting to mysql
 $db_conn = mysqli_connect ($host, $user, $dbpassword, $db);
 if($db_conn-> connect_error)
     die("Connection failure: ". $db_conn -> connect_error);
 
 
-//Students clicks button with id exam to take it
-//$exam = $_POST['exam'];
-//if (isset($_POST['exam'])){
-  //  $student_answers = "INSERT INTO StudentAnswers (exam, username, answer, total, comments)
-    //               VALUES ('$exam','null','null', 'null', 'null')";
-//}
+
+
+$exam = $_POST['exam'];
+$stu_username = $_POST['student'];
+$comments = unserialize($_POST['comments']);
+$student_answers = unserialize($_POST['studentanswers']);
+foreach($comments as $value){
+    $update_grades = "UPDATE Grades
+                         SET grade = '$grade', comments = '$value', student_answers = '$student_answers[$i]'
+                        WHERE username = '$stu_username'";
+    $i = $i+1;
+}
 
 $db_conn->close();
 
