b020eb86f190caddcda00ddab7e8bd1807c40915
ashmac27 <44986581+ashmac27@users.noreply.github.com>
2020-06-03 23:26:12 -0400

    Update takingExam.php

--- beta/backend/takingExam.php
+++ beta/backend/takingExam.php
@@ -5,13 +5,14 @@ $host = "sql1.njit.edu";
 $user = "am2829";
 $dbpassword = "urLNjFMv";
 $db  = "am2829";
-
 //Connecting to mysql
 $db_conn = mysqli_connect ($host, $user, $dbpassword, $db);
 if($db_conn-> connect_error)
     die("Connection failure: ". $db_conn -> connect_error);
 
 
+
+    
 //Students clicks button with id exam to take it
 $exam = $_POST['exam'];
 if (isset($_POST['exam'])){
@@ -19,23 +20,16 @@ if (isset($_POST['exam'])){
                    VALUES ('$exam','null','null', 'null', 'null')";
     $saq = $db_conn->query($student_answers);
 
-    
-    //$exam = mysqli_real_escape_string($_POST['exam']);
 
     $selecting_q = "SELECT questions FROM Exams WHERE exam = '$exam'";
     $result = $db_conn->query($selecting_q);
     $row = mysqli_fetch_row($result);
     $questions = explode(", ", $row[0]);
-   // $points_array = explode(", ", $row[1]);
-
 
     $row_arr = array();
-   // $j = 0;
     foreach ($questions as $i) {
         $new_query = "SELECT * FROM Questions WHERE question = '$question[$i]';";
         $new_query_result = $db_conn->query($new_query_result);
-        //$row = mysqli_fetch_assoc($new_query_result);
-        //$row = $sr_query->fetch_assoc();
         while ($row = $new_query_result->fetch_assoc()) {
             $row_arr[$i]['topic'] = $row['topic'];
             $row_arr[$i]['difficulty'] = $row['difficulty'];
@@ -44,8 +38,6 @@ if (isset($_POST['exam'])){
             $row_arr[$i]['tc2'] = $row['tc2'];
             $row_arr[$i]['answer1'] = $row['answer1'];
             $row_arr[$i]['answer2'] = $row['answer2'];
-            //$row_arr[$i]['points'] = $pt_array[$j];
-           // $j++;
         }
     }
     //returns a an array that could be displayed and formatted correctly on the screen
@@ -54,7 +46,6 @@ if (isset($_POST['exam'])){
 
 $username = $_POST['username'];
 $student_answer = $_POST['answer'];
-
 
 $db_conn->close();
 ?>
